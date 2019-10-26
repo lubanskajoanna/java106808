ef7b0b8fc34b6ad9b6d9f73468f2eb138cb54f87
lubanskajoanna <48630512+lubanskajoanna@users.noreply.github.com>
2019-10-26 12:21:59 +0200

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.util.Scanner;
+
+public class hello {
+		String str = "dfhgfdsdg";
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		public static void main(String[] args) {
+			
+			System.out.print("Hello World!");
+			
+			String hello = "Hello World!";
+			
+			System.out.print("Napis 'Hello World' ma " + hello.length() + " znakow.");
+			
+			
+		}
+}
