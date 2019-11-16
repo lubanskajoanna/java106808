3c4814b18b5925935ca2b14e87d30bca02c6417f
lubanskajoanna <48630512+lubanskajoanna@users.noreply.github.com>
2019-11-16 14:07:05 +0100

    Create Zadanie.java

---
+++ zjazd2/zadanie2/Zadanie.java
@@ -0,0 +1,15 @@
+public class Zadanie
+{
+	public static void main(String[] args)
+	{
+		public class Square implements Figure, Resetable
+		{
+			private int liczbBokow;
+			private boolean czyProstokat;
+		}
+    
+		Square pierwsza = new Square();
+    pierwsza.resetAllAttributes();
+		pierwsza.printSidesNumber();
+	}
+}
