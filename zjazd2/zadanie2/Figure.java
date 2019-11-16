4c22092d98812ceef4cc3108a0eb243dd9f99c57
lubanskajoanna <48630512+lubanskajoanna@users.noreply.github.com>
2019-11-16 14:03:14 +0100

    Create Figure.java

---
+++ zjazd2/zadanie2/Figure.java
@@ -0,0 +1,9 @@
+public class Figure implements NumbersOfSidesPrintable
+{
+	private boolean czyProstokat;
+  private int liczbBokow;
+	for (int i = 0; i < liczbBokow; i++)
+	{
+		int figura[i];
+	}
+}
