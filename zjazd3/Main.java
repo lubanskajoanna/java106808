f90b8df5f0846da78349f1debbb6398836087559
lubanskajoanna <48630512+lubanskajoanna@users.noreply.github.com>
2019-12-07 13:43:00 +0100

    Create Main.java

---
+++ zjazd3/Main.java
@@ -0,0 +1,28 @@
+public class Box<T> {
+    private T item;
+    
+    public Box(T item) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+    
+
+public class Main {
+  
+  public void contains()
+  
+  public static void main(String[] args) {
+    String[] array = {"element", "item"};
+    String element1 = "element";
+    String element2 = "element2";
+    
+    Main main = new Main();
+    System.out.println(main.contains(array, element1));
+    System.out.println(main.contains(array, element2));
+    
+    
+  }
+}
