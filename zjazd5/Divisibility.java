72daec35f2f5e6f8c0db418d071c5cfa5ed54fec
lubanskajoanna <48630512+lubanskajoanna@users.noreply.github.com>
2020-01-25 10:24:13 +0100

    Update Divisibility.java

--- zjazd5/Divisibility.java
+++ zjazd5/Divisibility.java
@@ -1,1 +1,32 @@
-
+class Divisibility extends Thread{
+  
+	public void function() {
+    
+		 for (int i=0;i<=100;i++) {
+			if (i%3==0) {
+				System.out.println(i);
+			}
+		} for (int i=101;i<=200;i++) {
+			if (i%3==0) {
+				System.out.println(i);
+			}
+		} for (int i=201;i<=300;i++) {
+			if (i%3==0) {
+				System.out.println(i);
+			}
+		} for (int i=301;i<=400;i++) {
+			if (i%3==0) {
+				System.out.println(i);
+			}
+		} for (int i=401;i<=500;i++) {
+			if (i%3==0) {
+				System.out.println(i);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Divisibility x = new Divisibility();
+		x.function();
+	    }
+}
